d4eea4d8abd25a628091bb2e552d98c9f690fd17
Fernando Perry <fernandoperry1234@gmail.com>
2025-07-12 12:53:46 +0700

    fix: should be the chat creation

--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import datetime
+from supabase import Client
 
-def chat_helper(supabase, user, query, hist_id):
+def chat_helper(supabase: Client, user, query, hist_id):
+    id_to_use = hist_id
     if hist_id is None: # create new hist
         try:
             response = (
@@ -14,10 +16,27 @@ def chat_helper(supabase, user, query, hist_id):
                 )
                 .execute()
             )
-            # TODO: create a new chat and call the ai stuff.
-            return {"code": 200, "data": response}
+            id_to_use = response.data[0].get("id")
         except Exception as e:
             return {"code": 500, "data": str(e)}
-    else:
-        # TODO: Finish this
-        pass
+
+    if id_to_use is None:
+        return {"code": 401, "data": "Invalid hist id"}
+
+    # TODO: actually call the ai.
+    ai_response = ""
+    try:
+        response = (
+            supabase.table("chat")
+            .insert(
+                {
+                    "history_id": id_to_use,
+                    "query": query,
+                    "response": "WIP",
+                    "created_at": datetime.datetime.now(),
+                }
+            ).execute()
+        )
+        return {"code": 200, "data": ai_response}
+    except Exception as e:
+        return {"code": 500, "data": str(e)}
